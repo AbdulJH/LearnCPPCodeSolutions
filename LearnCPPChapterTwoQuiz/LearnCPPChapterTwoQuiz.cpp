ab925b59d26922a72a1c1f5b5e3996874cff9123
Abdul Hussein <abdul.chs21@gmail.com>
2026-01-15 23:57:09 -0600

    question 1 quiz of 4.12 in learncpp

--- LearnCPPChapterTwoQuiz/LearnCPPChapterTwoQuiz.cpp
+++ LearnCPPChapterTwoQuiz/LearnCPPChapterTwoQuiz.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-#include "MYIO.H"
-
-
-
-int main()
-{
-    int firstNum{ readNumber() };
-    int secondNum{ readNumber() };
-
-    writeAnswer(firstNum + secondNum);
-
-
-    return 0;
-    
-}
-
+//#include <iostream>
+//#include "MYIO.H"
+//
+//
+//
+//int main()
+//{
+//    int firstNum{ readNumber() };
+//    int secondNum{ readNumber() };
+//
+//    writeAnswer(firstNum + secondNum);
+//
+//
+//    return 0;
+//    
+//}
+//
---
+++ LearnCPPChapterTwoQuiz/chapter4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// Write a short program where the user is asked to enter a single character. 
+// Print the value of the character and its ASCII code, using static_cast.
+// The program’s output should match the following :
+
+
+
+
+int main()
+{
+	std::cout << "Enter a single character: ";
+	char userInput{};
+	std::cin >> userInput;
+
+	std::cout << "You entered " << "'" << userInput << "', which has ASCII CODE " << static_cast<double>(userInput);
+
+
+
+
+
+
+	return 0;
+}
