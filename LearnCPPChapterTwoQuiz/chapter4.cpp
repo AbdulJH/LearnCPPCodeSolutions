03395d6a5c26790830453683495f8969a1e54b67
Abdul Hussein <abdul.chs21@gmail.com>
2026-01-16 00:19:26 -0600

    question 2 of chapter 4 summary code

--- LearnCPPChapterTwoQuiz/chapter4.cpp
+++ LearnCPPChapterTwoQuiz/chapter4.cpp
@@ -1,26 +1,65 @@
 #include <iostream>
 
-// Write a short program where the user is asked to enter a single character. 
-// Print the value of the character and its ASCII code, using static_cast.
-// The program’s output should match the following :
+// Write the following program: The user is asked to enter 2 floating point numbers (use doubles). 
+// The user is then asked to enter one of the following mathematical symbols: +, -, *, or /. 
+// The program computes the answer on the two numbers the user entered and prints the results. 
+// If the user enters an invalid symbol, the program should print nothing.
+
+
+double add(double num1, double num2) {
+	return num1 + num2;
+}
+
+double subtract(double num1, double num2) {
+	return num1 - num2;
+}
+
+double multiply(double num1, double num2) {
+	return num1 * num2;
+}
+
+double divide(double num1, double num2) {
+	return num1 / num2;
+}
+
+
+void checkResult(char userSymbol, double userNumOne, double userNumTwo) {
+	if (userSymbol == '+') {
+		std::cout << userNumOne << " + " << userNumTwo << " is " << add(userNumOne, userNumTwo);
+	}
+	else if (userSymbol == '-') {
+		std::cout << userNumOne << " - " << userNumTwo << " is " << subtract(userNumOne, userNumTwo);
+	}
+	else if (userSymbol == '*') {
+		std::cout << userNumOne << " * " << userNumTwo << " is " << multiply(userNumOne, userNumTwo);
+	}
+	else if (userSymbol == '/') {
+		std::cout << userNumOne << " / " << userNumTwo << " is " << divide(userNumOne, userNumTwo);
+	}
+	else {
+		std::cout << " ";
+	}
+
+}
 
 
 
 
 int main()
 {
-	std::cout << "Enter a single character: ";
-	char userInput{};
-	std::cin >> userInput;
-	int asciNum = userInput;
+	std::cout << "Enter a double value: ";
+	double userNumOne{};
+	std::cin >> userNumOne;
 
-	std::cout << "You entered " << "'" << userInput << "', which has ASCII CODE " << static_cast<int>(userInput);
-	std::cout << "You entered " << "'" << userInput << "', which has ASCII CODE " << asciNum;
+	std::cout << "Enter a double value: ";
+	double userNumTwo{};
+	std::cin >> userNumTwo;
 
+	std::cout << "Enter +, -, *, or /: ";
+	char userSymbol{};
+	std::cin >> userSymbol;
 
-
-
-
+	checkResult(userSymbol, userNumOne, userNumTwo);
 
 
 	return 0;
